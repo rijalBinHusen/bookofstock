41a2e39b2f0c0153286156bd58923bef75096123
rijal <jeryzal333@gmail.com>
2021-09-22 08:14:00 +0700

    Button close modal move to top right

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="my-modal" class="modal bg-base-200">
+    <a
+      href="#"
+      v-if="form !== 'ListGudang'"
+      class="btn fixed btn-secondary right-0 top-0 m-10 btn-lg justify-self-end"
+    >
+      <font-awesome-icon icon="times-circle" />
+    </a>
     <div class="w-full mx-20 my-2 p-2">
-      <a
-        href="#"
-        v-if="form !== 'ListGudang'"
-        class="btn fixed btn-secondary right-0 mr-10 btn-lg justify-self-end"
-      >
-        <font-awesome-icon icon="times-circle" />
-      </a>
       <component :is="form"></component>
     </div>
   </div>
